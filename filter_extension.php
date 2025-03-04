dee2689d65e844e00c6351e06c5886bb09ecc124
Keyor <jerryzhujian9@gmail.com>
2025-03-03 20:26:44 -0600

    update

--- filter_extension.php
+++ filter_extension.php
@@ -3,8 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 // Report all errors except deprecation warnings
-// error_reporting(E_ALL & ~E_DEPRECATED);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 class FilterTitleExtension extends Minz_Extension {
     public function init(): void {
